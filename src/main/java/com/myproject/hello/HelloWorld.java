f285e3a32401d8fea4b15e50b137c2f2b9505160
Moulali <moulalimoula190@gmail.com>
2024-11-08 15:24:14 +0530

    solution

---
+++ src/main/java/com/myproject/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.myproject.hello;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World moulali moulali moulali";
+    }
+}
